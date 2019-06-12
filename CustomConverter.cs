b4af8117be1df0910214fab40714c340609ef317
Ans Bilal <ansbilalabc@gmail.com>
2019-06-12 11:54:20 +0500

    Create CustomConverter.cs

---
+++ CustomConverter.cs
@@ -0,0 +1,56 @@
+public static class CustomConverter
+    {
+        public static string ToString(Object obj)
+        {
+            return Convert.ToString(obj);
+        }
+
+        public static int ToInt32(Object obj)
+        {
+            Int32 result = 0;
+            Int32.TryParse(Convert.ToString(obj), out result);
+            return result;
+        }
+
+        public static long ToInt64(Object obj)
+        {
+            Int64 result = 0;
+            Int64.TryParse(Convert.ToString(obj), out result);
+            return result;
+        }
+
+        public static short ToInt16(object obj)
+        {
+            Int16 result = 0;
+            Int16.TryParse(Convert.ToString(obj), out result);
+            return result;
+        }
+
+        public static decimal ToDecimal(object obj)
+        {
+            decimal result = 0;
+            decimal.TryParse(Convert.ToString(obj), out result);
+            return result;
+        }
+
+        public static DateTime ToDateTime(object obj)
+        {
+            DateTime result;
+            DateTime.TryParse(Convert.ToString(obj), out result);
+            return result;
+        }
+
+        public static double ToDouble(object obj)
+        {
+            Double result = 0;
+            Double.TryParse(Convert.ToString(obj), out result);
+            return result;
+        }
+
+        public static DateTime ToDateTimeFromUnix(object obj)
+        {
+            double seconds = ToDouble(obj);
+            DateTime origin = new DateTime(1970, 1, 1, 0, 0, 0, 0);
+            return origin.AddSeconds(seconds);
+        }
+    }
