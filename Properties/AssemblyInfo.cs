7bf5ee507b1ca7716cb3d2e841f79e61ae40f2d8
Ans bilal <ans.bilal@sixlogics.com>
2020-03-12 19:50:20 +0500

    build published

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -1,15 +1,16 @@
-﻿using System.Reflection;
+﻿using System.Resources;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("try-parse")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("TryParse")]
+[assembly: AssemblyDescription("This is a simple package that will Convert data into different data types safely.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("try-parse")]
+[assembly: AssemblyCompany("Team Alpha")]
+[assembly: AssemblyProduct("TryParse")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -34,3 +35,5 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguage("")]
+
